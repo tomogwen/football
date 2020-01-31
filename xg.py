fd9c2113483f75fa7d7cee28095c3b7aa8663816
tomogwen <tom.davies.96@hotmail.co.uk>
2020-01-31 17:12:11 +0000

    evaluating xG

--- xg.py
+++ xg.py
@@ -9,10 +9,13 @@ import seaborn as sns
 
 def generate_id_dicts(teams):
     for i in range(len(teams)):
-        if teams[i]['area']['name'] == "England":
+        """if teams[i]['area']['name'] == "England":
             print(str(teams[i]['wyId']) + ": \"" + teams[i]['name'] +"\",")
         if teams[i]['area']['name'] == "Wales":
-            print(str(teams[i]['wyId']) + ": \"" + teams[i]['name'] +"\".")
+            print(str(teams[i]['wyId']) + ": \"" + teams[i]['name'] +"\",")"""
+    for i in range(len(teams)):
+        # print("\"" + str(teams[i]['wyId']) + "\": \"" + teams[i]['name'] + "\",")
+        print("\"" + str(teams[i]['wyId']) + "\": \"" + str(i) + "\",")
 
 
 def generate_xg_pos(shots):
@@ -122,19 +125,87 @@ def create_xG_cont(xG, upper_bound=1):
     return xG_cont
 
 
+def plot_xG_G(x_plot, y_plot):
+    fig, ax = plt.subplots()
+    ax.scatter(x_plot, y_plot)
+
+    x_str = np.linspace(0, 110, 110)
+    y_str = np.linspace(0, 110, 110)
+
+    ax.plot(x_str, y_str, 'r')
+
+    ax.set_title('xG vs Goals Scored')
+
+    ax.set_xlabel('Goals')
+    ax.set_ylabel('xG')
+
+    return fig, ax
+
+
+def evaluate_xG_metric(shots, team_name_dict, team_id_dict, xG_scale=1.0):
+    teams_goals = np.zeros(len(team_name_dict))
+    teams_xg = np.zeros(len(team_name_dict))
+    teams_names = np.zeros(len(team_name_dict), dtype=object)
+
+    for i in range(len(shots)):
+        wyId = str(shots[i]['teamId'])
+        temp_id = int(team_id_dict[wyId])
+        teams_names[temp_id] = team_name_dict[wyId]
+
+        x_temp = shots[i]['positions'][0]['x'] * 1.04
+        y_temp = shots[i]['positions'][0]['y'] * 0.68
+
+        x, y = math.floor(x_temp) - 1, math.floor(y_temp) - 1
+
+        teams_xg[temp_id] += xG_scale * xG[x][y]
+        if shots[i]['tags'][0]['id'] == 101:
+            teams_goals[temp_id] += 1
+
+    error = 0
+    oe_total = 0
+    print()
+
+    x_plot, y_plot = [], []
+    for i in range(len(team_name_dict)):
+        if teams_xg[i] != 0:
+            temp_est = teams_goals[i] / teams_xg[i]
+        else:
+            temp_est = 0
+        oe_total += temp_est
+
+        x_plot.append(teams_goals[i])
+        y_plot.append(teams_xg[i])
+
+        print(teams_names[i] + " - xG: " + str(teams_xg[i]) + ", G: " + str(teams_goals[i]) + ", O/E: " +
+              str(temp_est))
+        error += abs(teams_xg[i] - teams_goals[i])
+
+    print("\nError: " + str(error))
+    oe_average = oe_total / len(team_name_dict)
+    print("Overestimate avg: " + str(oe_average))
+
+    fig, ax = plot_xG_G(x_plot, y_plot)
+    plt.show()
+    return error
+
+
 if __name__ == "__main__":
 
     xG = np.load("models/xg_cont.npy")
     xGfk = np.load("models/xGfk.npy")
 
+    # JUST EPL DATA
     matches, events, players, teams = dh.import_data("England")
+    team_name_dict, team_id_dict = dh.id_eng_dicts()
+
+    # ALL COMPETITIONS DATA
     # matches, events, players, teams = dh.import_all()
+    # team_name_dict, team_id_dict = dh.team_id_dicts()
 
-    # shots, pens, DFKs = get_shots(events)
-
+    # GENERATE XG MATRICES
     # xG, xGpen, xGdfk = generate_xG_values(shots, pens DFKs)
     # xG_cont = create_xG_cont(xG)
 
-    # wyId_id, wyId_name = dh.id_dicts()
+    shots, pens, DFKs = get_shots(events)
 
-    plot_xg(xG)
+    evaluate_xG_metric(shots, team_name_dict, team_id_dict, 0.6)
